abc8a62f89ab19bfdc1544c920b7c8f392958a27
Juan Javier Cabanas <jca@MacBook-Pro-de-Iago.local>
2016-01-21 12:08:09 +0100

    updated sample

--- sample/src/main/java/com/videonasocialmedia/videonarecorder/sample/RecorderSample.java
+++ sample/src/main/java/com/videonasocialmedia/videonarecorder/sample/RecorderSample.java
@@ -9,8 +9,10 @@ import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 
 import com.videonasocialmedia.avrecorder.AVRecorder;
+import com.videonasocialmedia.avrecorder.FullFrameRect;
 import com.videonasocialmedia.avrecorder.SessionConfig;
 import com.videonasocialmedia.avrecorder.event.CameraEncoderResetEvent;
+import com.videonasocialmedia.avrecorder.event.CameraOpenedEvent;
 import com.videonasocialmedia.avrecorder.view.GLCameraEncoderView;
 import com.videonasocialmedia.videonarecorder.R;
 
@@ -24,23 +26,31 @@ import de.greenrobot.event.EventBus;
  */
 public class RecorderSample extends Activity {
 
-    private final static String FOLDER= Environment.getExternalStoragePublicDirectory(
+    private final static String FOLDER = Environment.getExternalStoragePublicDirectory(
             Environment.DIRECTORY_MOVIES) + File.separator + "VideonaRecoder Sample";
 
     private AVRecorder recorder;
     private boolean firstTimeRecording;
+    GLCameraEncoderView cameraPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recorder_activity);
         initRecordButton();
-        SessionConfig config = new SessionConfig(FOLDER+System.currentTimeMillis()+".pm4");
+        initRotateButton();
+
+        cameraPreview = (GLCameraEncoderView) findViewById(R.id.cameraPreview);
+        //SessionConfig config = new SessionConfig(FOLDER+System.currentTimeMillis()+".pm4");
+        SessionConfig config = new SessionConfig(FOLDER,
+                640, 480, 5 * 1000 * 1000, 1, 48000, 192000);
         try {
             recorder = new AVRecorder(config);
+
             GLCameraEncoderView cameraPreview =
                     (GLCameraEncoderView) findViewById(R.id.cameraPreview);
             recorder.setPreviewDisplay(cameraPreview);
+
             firstTimeRecording = true;
         } catch (IOException e) {
             Log.e("RecorderSample", e.getMessage());
@@ -57,11 +67,12 @@ public class RecorderSample extends Activity {
     protected void onResume() {
         super.onResume();
         EventBus.getDefault().register(this);
+
     }
 
     @Override
     protected void onPause() {
-        super.onResume();
+        super.onPause();
         EventBus.getDefault().unregister(this);
     }
 
@@ -86,7 +97,21 @@ public class RecorderSample extends Activity {
         });
     }
 
-    private void startRecord(){
+    private void initRotateButton() {
+        ToggleButton button = (ToggleButton) findViewById(R.id.rotateCameraButton);
+        button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    recorder.signalVerticalVideo(FullFrameRect.SCREEN_ROTATION.VERTICAL);
+                else
+                    recorder.signalVerticalVideo(FullFrameRect.SCREEN_ROTATION.LANDSCAPE);
+            }
+        });
+    }
+
+
+    private void startRecord() {
         if (!recorder.isRecording()) {
             if (!firstTimeRecording) {
                 try {
@@ -101,13 +126,13 @@ public class RecorderSample extends Activity {
     }
 
     private void resetRecorder() throws IOException {
-        SessionConfig config = new SessionConfig(FOLDER+System.currentTimeMillis()+".pm4");
+        SessionConfig config = new SessionConfig(FOLDER + System.currentTimeMillis() + ".mp4",
+                640, 480, 5 * 1000 * 1000, 1, 48000, 192000);
         recorder.reset(config);
     }
 
     public void onEventMainThread(CameraEncoderResetEvent e) {
         startRecord();
     }
-
 
 }
